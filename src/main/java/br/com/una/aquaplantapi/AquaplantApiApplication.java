d9665220980bf28ad7daa6c15da51f3742953563
roger <fontestz@gmail.com>
2024-04-18 20:13:36 -0300

    aula 18/04

--- src/main/java/br/com/una/aquaplantapi/AquaplantApiApplication.java
+++ src/main/java/br/com/una/aquaplantapi/AquaplantApiApplication.java
@@ -3,16 +3,22 @@ package br.com.una.aquaplantapi;
 import br.com.una.aquaplantapi.model.domain.Cliente;
 import br.com.una.aquaplantapi.model.domain.Planta;
 import br.com.una.aquaplantapi.model.repository.ClienteRepositorio;
+import br.com.una.aquaplantapi.model.repository.PlantaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class AquaplantApiApplication implements CommandLineRunner {
 
 	@Autowired
 	private ClienteRepositorio clienteRepositorio;
+
+	@Autowired
+	private PlantaRepositorio plantaRepositorio;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AquaplantApiApplication.class, args);
@@ -30,6 +36,13 @@ public class AquaplantApiApplication implements CommandLineRunner {
 		Planta planta = new Planta();
 		planta.setNome("Rosa");
 		planta.setCliente(clienteSalvo);
+
+		plantaRepositorio.save(planta);
+
+		List<Planta> plantaList = plantaRepositorio.findAll();
+		for(Planta p : plantaList){
+			System.out.println(p);
+		}
 
 	}
 }
